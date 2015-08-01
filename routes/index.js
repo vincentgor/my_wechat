e36a94813ae3e07382eee0b42cac262dfc56c5e5
vinxent <1149536116>
2015-08-01 18:01:30 +0800

    修bug

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ var xml2js = require('xml2js');
 var parseString = xml2js.parseString;
 
 var conf = require('../conf').conf;
-var T = require('./model/textMessageReq').textMessageReq;
+var T = require('../model/textMessageReq').textMessageReq;
 
 var router = express.Router();
 
