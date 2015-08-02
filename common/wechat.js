a9a545e3762a34141b825a57ff1ee2a4472f2bc0
vinxent <1149536116>
2015-08-02 16:50:58 +0800

    增加微信处理模块，优化代码。

---
+++ common/wechat.js
@@ -0,0 +1,44 @@
+/**
+ * Created by vinxent on 2015/8/2.
+ * 微信模块
+ */
+var crypto = require('crypto');
+var builder = new xml2js.Builder({rootName: 'xml', headless:true});   //去掉xml格式说明
+
+var conf = require('../conf').conf;
+var TReq = require('../model/messageReq/textMessageReq').textMessageReq;
+var TResp = require('../model/messageResp/textMessageResp').textMessageResp;
+
+//微信处理类
+var Wechat = function() {
+
+}
+
+/* 验证微信消息的合法性 */
+Wechat.check = function (timestamp, nonce, signature) {
+  var tmpArr =[conf.TOKEN,timestamp,nonce];
+  tmpArr.sort();
+  var str = tmpArr.join('');
+  console.log('排序后： '+str);
+  var sha1 = crypto.createHash('sha1');
+  sha1.update(str);
+  var checkedStr = sha1.digest('hex');
+  console.log('sha1加密后:  ' + checkedStr);
+  return checkedStr==signature;
+};
+
+Wechat.process = function () {
+  var textReq = TReq.init(result.xml);
+
+  //逻辑处理
+  textReq.Content = '你发送的是： ' + textReq.MsgType; + ' 消息啦啦啦'
+  textReq.MsgType = 'text';
+
+  //响应消息
+  var textResp = TResp.init(textReq);
+  console.log(textResp);
+  var xml = builder.buildObject(textResp);
+  return xml;
+};
+
+exports.wechat = Wechat;
--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,11 @@
 var express = require('express');
-var crypto = require('crypto');
 var xml2js = require('xml2js');
 var parseString = xml2js.parseString;
 
-var conf = require('../conf').conf;
-var TReq = require('../model/messageReq/textMessageReq').textMessageReq;
-var TResp = require('../model/messageResp/textMessageResp').textMessageResp;
 
-var parse = new xml2js.Parser;
-var builder = new xml2js.Builder({rootName: 'xml', headless:true});   //去掉xml格式说明
+var wechat = require('../common/wechat').wechat;
+
+
 
 var router = express.Router();
 
@@ -19,55 +16,30 @@ router.get('/', function(req, res, next) {
 
 /* 微信验证接口. */
 router.get('/wechat', function(req, res, next) {
+
   var signature = req.param('signature');   //微信加密签名
-  console.log('signature: ' + signature);
   var timestamp = req.param('timestamp');   //时间戳
-  console.log('timestamp: ' + timestamp);
   var nonce = req.param('nonce');            //随机数
-  console.log('nonce: ' + nonce);
   var echostr = req.param('echostr');        //随机字符串
-  console.log('echostr: ' + echostr);
-  var tmpArr =[conf.TOKEN,timestamp,nonce];
-  tmpArr.sort();
-  var str = tmpArr.join('');
-  console.log('排序后：'+str);
-  var shasum = crypto.createHash('sha1');
-  shasum.update(str);
-  var d = shasum.digest('hex');
-  console.log(d);
-  if(d==signature) {
-    res.end(echostr);
+
+  if(wechat.check(timestamp, nonce, signature)) {
+    res.end(echostr);     //通过
   } else {
-    console.log('error');
-    res.end('error');
+    res.end('error');   //失败
   }
-  //res.render('index', { title: 'Express' });
 });
 
 /* 微信消息处理接口 */
 router.post('/wechat', function (req, res, nect) {
-  var tempData = '';
+  var tempData = '';  //接收到的消息
   req.on('data', function (chunk) {
     tempData += chunk;
   })
   req.on('end', function() {
-    console.log(tempData);
+    console.log('接收到的消息xml为： ' + tempData);
     parseString(tempData, {explicitArray: false, trim: true}, function (err, result) {
-      var textReq = TReq.init(result.xml);
-      console.log(result);
-      console.log(textReq);
-
-      //逻辑处理
-      textReq.Content = '你发送的是： ' + textReq.MsgType;
-      textReq.MsgType = 'text';
-
-      //响应消息
-      var textResp = TResp.init(textReq);
-      console.log(textResp);
-      var xml = builder.buildObject(textResp);
-      console.log(xml);
+      var xml = wechat.process(result);    //处理后的xml
       res.end(xml);
-      return;
     });
   });
 });
